eedd7c4ba93b3ae0a56271257d9d83e470068ad9
TapashIT07 <tapashit07@gmail.com>
2023-09-18 19:16:14 +0600

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,35 @@
 
+// $(function(){
+//     alert('ok')
+
+//     $('#bars').click(function(){
+//         $('.menu_con ul ').toggleClass('top_80')
+//     })
+
+
+//     $('.back_to_top').click (function(){
+//         $('html,body').animate({scrollTop: 0},500)
+//     })
+
+//     $(window).scroll(function(){
+//     var scrollTop = $(this).scrollTop();
+
+//     if(scrollTop>500){
+//         $('.back_to_top').show(500);
+//     } else {
+//         $('.back_to_top').hide(500);  
+//     }
+
+//     })
+
+// });
+
 $(function(){
 
-    $('#bars').click(function(){
-        $('.menu_con ul ').toggleClass('top_80')
-    })
-
-
-    $('.back_to_top').click (function(){
-        $('html,body').animate({scrollTop: 0},500)
-    })
-
-    $(window).scroll(function(){
-    var scrollTop = $(this).scrollTop();
-
-    if(scrollTop>500){
-        $('.back_to_top').show(500);
-    } else {
-        $('.back_to_top').hide(500);  
-    }
-
-    })
+$('.bars').click(function(){
+    $('.menu').slideToggle();
 
 });
 
+});
 
