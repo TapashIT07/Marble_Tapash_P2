d58783d638e898c775e070356312adce6f8ce083
TapashIT07 <tapashit07@gmail.com>
2023-09-20 18:03:08 +0600

    This is a commit on 20-09-23

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,5 +31,27 @@ $('.bars').click(function(){
 
 });
 
+
+$('.back_to_top').click(function(){
+    $('html,body').animate({scrollTop:0},500);
+
+});
+
+
+$(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+
+    if (scrollTop>400){
+        $('.back_to_top').show();
+    } else{
+        
+        $('.back_to_top').hide();
+
+    }
+});
+
+
+
+
 });
 
